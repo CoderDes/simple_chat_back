0e8ce58a1e08347a2d113e33e7294a85583fc76e
Eugene <eugzubv@gmail.com>
2020-10-09 21:54:23 +0300

    create user successfully

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,5 +1,16 @@
-// import UserModel from "users.model";
+import { Request, Response } from "express";
+import UserModel from "./users.model";
 
-export const registerUser = async (req: object, res: object) => {};
+export const registerUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = new UserModel({ email, password });
+    await user.save();
 
-export const loginUser = async (req: object, res: object) => {};
+    res.send({ message: "user created successfully", id: user._id });
+  } catch (err) {
+    res.send("Fail " + err);
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {};
